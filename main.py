e3750e7beea09503326b08a1ebe4d0708f82d2b4
manuel <manuel@danielcastelao.org>
2020-11-03 11:56:29 +0100

    Excepcions e módulos

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import novoModulo
+from excepcions import Punto, Cilindro
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# clase = novoModulo.UnhaClase()
+
+punto = Punto(2, 3)
+c = Cilindro(3, 4, 5)
+
+if __name__ == "__main__":
+    novoModulo.funcion()
